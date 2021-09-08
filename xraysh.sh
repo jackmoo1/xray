2d81dd9219e348aad70043dc881165f692084609
jackmoo <51739356+a31663835@users.noreply.github.com>
2021-09-08 10:55:01 +0800

    Update xraysh.sh

--- xraysh.sh
+++ xraysh.sh
@@ -284,7 +284,7 @@ getData() {
             real_addr=`ping ${DOMAIN} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
             local_addr=`curl v4.ident.me`
             if [[ $real_addr == $local_addr]]; then
-                colorEcho ${BLUE}  "${DOMAIN} 解析结果：${resolve}"
+                colorEcho ${BLUE}  "${DOMAIN} 解析结果：${real_addr}"
 	    else	
                 colorEcho ${RED}  " 域名未解析到当前服务器IP(${IP})!"
                 exit 1
