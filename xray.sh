ce315b10a75543ce3b7520f05e5cab916b3ed4aa
jackmoo <51739356+jackmoo1@users.noreply.github.com>
2021-11-05 20:29:02 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -2226,7 +2226,7 @@ menu() {
             showLog
             ;;
         18)
-            renewalTLS 1
+            checkTLStatus 1
             ;;
         19)
             dnsUnlock 1
@@ -2243,12 +2243,12 @@ checkSystem
 action=$1
 [[ -z $1 ]] && action=menu
 case "$action" in
-    menu|update|uninstall|start|restart|stop|showInfo|showLog|renewalTLS|dnsUnlock)
+    menu|update|uninstall|start|restart|stop|showInfo|showLog|checkTLStatus|dnsUnlock)
         ${action}
         ;;
     *)
         echo " 参数错误"
-        echo " 用法: `basename $0` [menu|update|uninstall|start|restart|stop|showInfo|showLog]"
+        echo " 用法: `basename $0` [menu|update|uninstall|start|restart|stop|showInfo|showLog|checkTLStatus|dnsUnlock]"
         ;;
 esac
 
